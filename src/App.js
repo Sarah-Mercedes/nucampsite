9864bb57197ca8c3322a7789546bf720d1b81cc3
Sarah Mercedes <sarahmercedes1@yahoo.com>
2021-12-01 12:47:45 -0500

    Loading and Errors with Redux Thunk

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import './App.css';
+
 const store = ConfigureStore();
+
 class App extends Component {
   render() {
       return (
